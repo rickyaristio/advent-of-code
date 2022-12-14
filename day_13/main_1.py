6d9f7298c119f3649035ed105e7e926a10509220
rickyaristio <ricky.aristio@gmail.com>
2022-12-14 09:53:14 +0100

    day 13 part 1 and 2

--- day_13/main_1.py
+++ day_13/main_1.py
@@ -6,9 +6,6 @@ def compareList(list_left,list_right):
                 list_left.append(0)
             elif(len(list_left) > len(list_right)):
                 list_right.append(0)
-        
-        # print(n, len(list_left), list_left, list_left[n])
-        # print(n, len(list_right), list_right, list_right[n])
 
         # integer vs integer
         if(isinstance(list_left[n], int)  == True and isinstance(list_right[n], int)  == True):
@@ -39,53 +36,25 @@ def compareList(list_left,list_right):
             elif compareList(list_left[n],list_right[n]) == 0:
                 return 0 
 
-def sortLists(lists):
-    for i in range(1,len(lists)):
-        temp = lists[i]
-        j = i -1
-
-        #compare temp with the next array
-        while j >= 0 and compareList(lists[j],temp):
-            lists[j+1] = lists[j]
-            j -= 1
-        
-        lists[j+1] = temp
-    return(lists)
-
 with open('input.txt', 'r') as f:
     lines = f.readlines()
 
-lists = []
+list_left = []
+list_right = []
+pairs = []
 for n in range(int((len(lines)+1)/3)):
 
-    lists.append(eval(lines[n*3]))
-    lists.append(eval(lines[n*3+1]))
+    list_left = eval(lines[n*3])
+    list_right = eval(lines[n*3+1])
+    
+    score = compareList(list_left,list_right)
+    if score == 0:
+        pairs.append(n+1)
 
-lists = sortLists(lists)
 
-#insert [[2]]
-pos_2 = 0
-lists.append([[2]])
-temp = lists[len(lists)-1]
-j = len(lists) -2
-while j >= 0 and compareList(lists[j],temp):
-    lists[j+1] = lists[j]
-    j -= 1
-    pos_2 += 1
-lists[j+1] = temp
+print(pairs)
+print(sum(pairs)) #6086
 
-#insert [[6]]
-pos_6 = 0
-lists.append([[6]])
-temp = lists[len(lists)-1]
-j = len(lists) -2
-while j >= 0 and compareList(lists[j],temp):
-    lists[j+1] = lists[j]
-    j -= 1
-    pos_6 += 1
-lists[j+1] = temp
 
-# print(lists)
-print(len(lists)-pos_2 - 1)
-print(len(lists)-pos_6)
-print((len(lists)-pos_2 - 1)*(len(lists)-pos_6))
+
+
