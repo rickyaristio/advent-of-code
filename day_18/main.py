6b6a5955db83a9bad00b2ea20fd012a5fd9ffcd6
Ricky Aristio <ricky.aristio@gmail.com>
2022-12-23 21:44:14 +0100

    day 18 part 2

--- day_18/main.py
+++ day_18/main.py
@@ -1,63 +1,50 @@
-import numpy as np
-np.set_printoptions(linewidth=np.inf)
-
-
-def checkNeighbour(number):
-    return 6-2*number
+#credits: https://github.com/jornpe/Advent-of-Code-Python/blob/main/2022
 
 with open('input.txt', 'r') as f:
     lines = f.readlines()
 
+cells = []
 x = []
 y = []
 z = []
 
 for line in lines:
     split_line = line.split(',')
+    xyz = (int(split_line[0]), int(split_line[1]), int(split_line[2]))
+    cells.append(xyz)
     x.append(int(split_line[0]))
     y.append(int(split_line[1]))
     z.append(int(split_line[2]))
 
 max_x = max(x)
 max_y = max(y)
-max = max(max_x,max_y)
+max_z = max(z)
+max = max(max_x,max_y,max_z)
 
+neighbours = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1)]
 
-#create a field
-rows = max + 1 
-cols = max + 1
-matrix_2d = [[0 for c in range(cols)] for r in range(rows)]
-for r in range(rows):
-  for c in range(cols):
-   matrix_2d[r][c]= []
+#part 1
+count = 0
+for x,y,z in cells:
+    for dx,dy,dz in neighbours:
+        if(x + dx, y + dy, z + dz) not in cells:
+            count += 1
+print(count) #3412
 
-sum = 0
-for n in range(len(lines)):
-    num_neighbor = 0
+#part 2
+count_2 = 0
+visited = set()
+queue = [(0,0,0)]
 
-    #check z dir
-    if not matrix_2d[x[n]][y[n]]: 
-        matrix_2d[x[n]][y[n]].append(n+1)
-    else:
-        for m in range(len(matrix_2d[x[n]][y[n]])):
-            if(abs(z[matrix_2d[x[n]][y[n]][m]-1] - z[n]) == 1):
-                num_neighbor += 1
-        matrix_2d[x[n]][y[n]].append(n+1)
-    
-    #check x-y dir
-    for di,dj in [(0,-1),(-1,0),(0,1),(1,0)]:
-        if x[n]+di < 0 or y[n]+dj < 0: #out of bounds
-            continue
-        if x[n]+di>=rows or y[n]+dj>=cols: #out of bounds
-            continue
-        if not matrix_2d[x[n]+di][y[n]+dj]:
-            continue
-        else:
-            for m in range(len(matrix_2d[x[n]+di][y[n]+dj])):
-                if(z[matrix_2d[x[n]+di][y[n]+dj][m]-1] == z[n]):
-                    num_neighbor += 1
-    
-    print('turn',n, num_neighbor, checkNeighbour(num_neighbor))
-    sum += checkNeighbour(num_neighbor)
+while queue:
+    x,y,z = queue.pop(0)
+    visited.add((x,y,z))
+    for dx,dy,dz in neighbours:
+        check = (x + dx, y + dy, z + dz)
+        if -1 <= check[0] <= max+1 and -1 <= check[1] <= max+1 and -1 <= check[2] <= max+1 and check not in visited and check not in queue:
+            if check in cells:
+                count_2 += 1
+            else:
+                queue.append(check)
 
-print(sum)
+print(count_2) #2018
